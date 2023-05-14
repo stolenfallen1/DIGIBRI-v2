0c2bfe6d3956f7d0e3c7cf9d45b7350a0d73a3c3
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-14 22:04:43 +0800

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="p-0 m-0 box-border">
+      <h1 className="font-bold text-2xl text-center">HELLO WORLD</h1>
+    </div>
+  );
 }
 
 export default App;
