5f848eb5c069648628941c6991ce5b90407dce4c
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-16 00:20:46 +0800

    sidebar partial  styling

--- src/Components/DashComponents/Sidebar.tsx
+++ src/Components/DashComponents/Sidebar.tsx
@@ -1,23 +1,35 @@
+import BLACK_LOGO from "../../Assets/Logo/BLACK_LOGO.png";
+
 const Sidebar = () => {
   return (
-    <div>
-      <main>
-        <header>DIGIBRI</header>
-        <ul>
-          <li>
-            <a>One</a>
+    <div
+      className="p-0 m-0 box-border"
+      style={{ width: "225px", height: "100vh" }}
+    >
+      <main className="flex flex-col h-full bg-black justify-center items-center">
+        <img
+          src={BLACK_LOGO}
+          alt="DIGIBRI"
+          style={{ width: "140px", height: "auto" }}
+        />
+        <ul className="text-white text-2xl flex flex-col items-center">
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Analytics</a>
           </li>
-          <li>
-            <a>Two</a>
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Category</a>
           </li>
-          <li>
-            <a>Three</a>
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Location Rack</a>
           </li>
-          <li>
-            <a>Four</a>
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Books</a>
           </li>
-          <li>
-            <a>Five</a>
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Issue Book</a>
+          </li>
+          <li className="py-4 cursor-pointer hover:text-gray-400">
+            <a>Library Patron</a>
           </li>
         </ul>
       </main>
