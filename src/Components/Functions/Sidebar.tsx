0de3212d8c6fda8330987fafe5c6600100490f82
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-06-19 23:28:45 +0800

    re-design sidebar

--- src/Components/Functions/Sidebar.tsx
+++ src/Components/Functions/Sidebar.tsx
@@ -7,14 +7,15 @@ const Sidebar = () => {
       style={{ width: "225px", height: "100vh" }}
     >
       <main className="flex flex-col h-full bg-black justify-center items-center">
-        <img
-          src={BLACK_LOGO}
-          alt="DIGIBRI"
-          style={{ width: "140px", height: "auto" }}
-        />
         <ul className="text-white text-2xl flex flex-col items-center">
-          <li className="py-4 cursor-pointer hover:text-gray-400">
-            <a>Analytics</a>
+          <li className="cursor-pointer">
+            <a>
+              <img
+                src={BLACK_LOGO}
+                alt="DIGIBRI"
+                style={{ width: "140px", height: "auto" }}
+              />
+            </a>
           </li>
           <li className="py-4 cursor-pointer hover:text-gray-400">
             <a>Category</a>
@@ -30,6 +31,9 @@ const Sidebar = () => {
           </li>
           <li className="py-4 cursor-pointer hover:text-gray-400">
             <a>Library Patron</a>
+          </li>
+          <li className="py-4 cursor-pointer hover:text-red-500">
+            <a>Logout</a>
           </li>
         </ul>
       </main>
